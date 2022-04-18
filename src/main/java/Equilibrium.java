0ffe232d33dd2c7ed50c95bcf7075cf42dcb7b17
Kalyan Chakravarthy V <KalyanChakravarthyV@gmail.com>
2022-04-18 08:38:44 +0530

    Draft Commit

---
+++ src/main/java/Equilibrium.java
@@ -0,0 +1,14 @@
+import com.tririga.pub.adapter.IConnect;
+import com.tririga.ws.TririgaWS;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class Equilibrium implements IConnect {
+    @Override
+    public void execute(TririgaWS tririgaWS, HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws ServletException, IOException {
+
+    }
+}
