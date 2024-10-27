cf7d5f8d75e151058dc5ec3e63685d8328d205d3
Kalyan Chakravarthy V <KalyanChakravarthyV@gmail.com>
2024-10-28 03:29:29 +0530

    Beta release for Q4

--- src/main/java/com/tririga/custom/Equilibrium.java
+++ src/main/java/com/tririga/custom/Equilibrium.java
@@ -2,6 +2,7 @@ package com.tririga.custom;
 
 import com.tririga.pub.adapter.IConnect;
 import com.tririga.ws.TririgaWS;
+import in.vadlakonda.equilibrium.api.APIException;
 import in.vadlakonda.equilibrium.dispatch.RequestDispatcherFactory;
 import in.vadlakonda.equilibrium.init.AppInitiliazer;
 import org.apache.log4j.Logger;
@@ -9,14 +10,17 @@ import org.apache.log4j.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.File;
 import java.io.IOException;
+import java.net.HttpURLConnection;
 
 public class Equilibrium implements IConnect {
 
     private static final org.apache.log4j.Logger log = Logger.getLogger(Equilibrium.class);
 
     private static final RequestDispatcherFactory requestDispatcherFactory;
+    private static final String TRIRIGA_WS_SESSION_KEY = "TririgaWS";
 
     static {
 
@@ -28,6 +32,11 @@ public class Equilibrium implements IConnect {
 
     @Override
     public void execute(TririgaWS tririgaWS, HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws ServletException, IOException {
+
+        HttpSession session = httpServletRequest.getSession(false);
+
+        if(session != null)
+            session.setAttribute(TRIRIGA_WS_SESSION_KEY, tririgaWS);
 
         requestDispatcherFactory.getRequestDispatcher(httpServletRequest).dispatch(httpServletRequest, httpServletResponse, AppInitiliazer.getAppClassLoader());
 
